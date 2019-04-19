b56e42905dd7b4f58ac924d7aaa3276789c6bda6
Luciano Mareco <49459750+lucianomareco@users.noreply.github.com>
2019-04-18 22:10:15 -0300

    Add files via upload

---
+++ 01-Adición/adicion.cpp
@@ -0,0 +1,15 @@
+/* LUCIANO GASTON MARECO
+ * 20190419
+ *  Adición
+ */
+
+#include <iostream>
+
+int main(){
+
+     int a;
+     std::cin >> a;
+     int b;
+     std::cin >> b;
+     std::cout << a + b;
+     }
