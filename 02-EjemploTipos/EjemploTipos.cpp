fbd6d2e90637b1bebeb8e749414b7b75ec9f1f9c
Luciano Mareco <49459750+lucianomareco@users.noreply.github.com>
2019-05-01 22:07:11 -0300

    Add files via upload

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,25 @@
+/* LUCIANO GASTON MARECO
+ * 20190501
+ *  EjemploTipos
+ */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main () {
+
+unsigned dia = 1;
+string mes = "mayo";
+char barra = '/';
+int anio = 2019;
+double temp = 14.5;
+bool precipitaciones = false;
+
+cout << "hoy es "<< dia << barra << mes << barra << anio << endl;
+cout << "la temperatura es "<< temp << endl;
+cout << "lluvia " << precipitaciones << endl;
+
+      
+}
